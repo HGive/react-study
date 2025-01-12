47baacf29ba596c7d4a0d87a94ebbe9fb8792f36
algorijun <123007169+HGive@users.noreply.github.com>
2025-01-12 16:56:33 +0900

    lecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import MainPage from '@pages/index'
+import MainPage from "@pages/index/index"
 
 function App() {
   return (
---
+++ src/components/common/footer/CommonFooter.tsx
@@ -0,0 +1,8 @@
+import styles from "./CommonFooter.module.scss"
+function CommonFooter() {
+  return (
+    <div>CommonFooter</div>
+  )
+}
+
+export default CommonFooter
